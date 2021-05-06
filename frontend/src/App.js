a8bbfea92c198e856b26189df37e8daf3c301461
ashur-k <ashurkanwal@yahoo.com>
2021-05-06 10:54:54 +0100

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/header/Header.jsx'
+import Footer from './components/footer/Footer.jsx'
+
+function App() {
+  return (
+    <div className="App">    
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>      
+      <Footer />     
+    </div>
+  );
+}
+
+export default App;
