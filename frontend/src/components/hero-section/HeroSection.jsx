bf538d42cb1b789a4b622dd48172a3c685066eae
ashur-k <ashurkanwal@yahoo.com>
2021-05-10 12:57:05 +0100

    added herosction

--- frontend/src/components/hero-section/HeroSection.jsx
+++ frontend/src/components/hero-section/HeroSection.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './hero.css'
+import { Jumbotron, Button } from 'react-bootstrap'
 
 function HeroSection() {
     return (
         <div>
-            <h1>Welcome Nick and Robin</h1>
+        <Jumbotron >
+        <h1 className="text-center">WEBSITE SOLUTION</h1>
+        <p className="text-center">
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p className="text-center">
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
         </div>
     )
 }
