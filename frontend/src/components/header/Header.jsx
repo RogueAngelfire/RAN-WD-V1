ec614a12adfc23311b72e68a260ede261bbe7ddf
Robin Collins <info@robincollins.co.uk>
2021-05-11 17:04:00 +0100

    Added price plan page and links

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -25,17 +25,14 @@ function Header() {
                                         <NavDropdown.Divider />
                                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                                     </NavDropdown>
-                                    <NavDropdown title="Price Plans" id="basic-nav-dropdown">
-                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                        <NavDropdown.Divider />
-                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                    </NavDropdown>
+                                    <LinkContainer to='/pricePlan'>
+                                        <Nav.Link>Price Plan</Nav.Link>
+                                    </LinkContainer> 
                                     <Nav.Link href="">Portfolio</Nav.Link> 
                                     <LinkContainer to='/about'>
                                         <Nav.Link>About</Nav.Link>
-                                    </LinkContainer>                       
+                                    </LinkContainer>  
+                                                         
                                     
                                     <Nav.Link href="">Contact</Nav.Link>                            
                                 </Nav> 
