d9c89cbaaba51493d5bc0390429fb92c2606bd24
ashur-k <ashurkanwal@yahoo.com>
2021-05-11 09:00:35 +0100

    added code to programmer details

--- frontend/src/screens/ProgrammerDetails.js
+++ frontend/src/screens/ProgrammerDetails.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, ListGroup, Button, Card } from 'react-bootstrap'
+
 
 function ProgrammerDetails() {
     return (
